3765f80c847e95c27b21df4d46945ab83b041730
dungnn1711 <dungnn1711@gmail.com>
2022-11-05 15:34:24 +0700

    Init project

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+interface AboutProps {}
+
+function About(props: AboutProps) {
+    return (
+        <>
+            This is About Page.
+        </>
+    );
+}
+
+export default About;
---
+++ pages/product/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { useState } from 'react';
+
+interface DetailProductProps { }
+
+function DetailProduct(props: DetailProductProps) {
+    return (
+        <>
+            This is detail product page.
+        </>
+    );
+}
+
+export default DetailProduct;
